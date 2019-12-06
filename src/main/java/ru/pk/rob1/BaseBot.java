326938348ba8558ce20bc3bec781c7b8870c24dd
pkprog <pk.prog@yandex.ru>
2019-12-06 23:49:29 +0300

    init

---
+++ src/main/java/ru/pk/rob1/BaseBot.java
@@ -0,0 +1,15 @@
+package ru.pk.rob1;
+
+import robocode.Robot;
+
+public class BaseBot extends Robot {
+    @Override
+    public String getName() {
+        return "pk-BaseBot1";
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
